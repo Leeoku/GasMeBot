0d96e2d578f870bb7435c93b087afcbd451e02fd
Kenneth Lee <leeoku@gmail.com>
2019-09-11 23:37:41 -0400

    Add Messaging

--- gasmebot.py
+++ gasmebot.py
@@ -3,8 +3,7 @@ import os
 import requests
 import json
 token = "639358022:AAHtST1qHa5OZRG7O4-Du4X5IZBflLaY-DQ"
-telegram_url = "https://api.telegram.org/bot{}/getupdates".format(token)
-print(telegram_url)
+
 url = "http://stockr.net/Toronto/GasPrice.aspx"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
@@ -12,26 +11,41 @@ soup = BeautifulSoup(page.text, 'html.parser')
 #oldPrice = soup.findAll("",{"id":"lPrice2"})
 #oldPrice = soup.findAll("",{"class":"gasPrice"})
 
-def get_url(url):
+def main(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
-    print(content)
+    return content
 
 
 def priceCompare():
-    oldPrice = soup.find("",{"id":"lPrice2"}).text
+    oldPrice = float(soup.find("",{"id":"lPrice2"}).text)
     oldDate = soup.find("",{"id":"lDate2"}).text
-    newPrice = soup.find("",{"id":"lPrice1"}).text
+    newPrice = float(soup.find("",{"id":"lPrice1"}).text)
     newDate = soup.find("",{"id":"lDate1"}).text
-
+    difference = abs(float(newPrice)-float(oldPrice))
+    text = ""
     if newPrice == "":
-        print("No update yet")
+        text="No update yet"
+        print(text)
+        return text
+    elif difference > 10:
+        text = "This value doesn't seem right"
+        print(text)
+        return text
     elif newPrice > oldPrice:
-        print("Tomorrow's price will increase by " + (abs(newPrice-oldPrice)) + "¢")
+        text= "Tomorrow's price will increase by " + str(difference) + "¢"
+        print(text)
+        return text
     elif newPrice < oldPrice:
-        print("Tomorrow's price will decrease by " + (abs(newPrice-oldPrice)) + "¢")
+        text = "Tomorrow's price will decrease by " + str(difference) + "¢"
+        print(text)
+        return text
     elif newPrice == oldPrice:
+        text = "No change."
         print("No change.")
+        return text
 
-priceCompare()
-get_url(telegram_url)
+final_text = priceCompare()
+telegram_url = "https://api.telegram.org/bot{}/sendMessage?chat_id=690011658&text={}".format(token,final_text)
+#print(telegram_url)
+main(telegram_url)
