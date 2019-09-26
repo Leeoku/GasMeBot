5632ee6fc50dda85ba6d57c54613968347784b3e
Kenneth Lee <leeoku@gmail.com>
2019-09-25 22:38:07 -0400

    modified main function

--- gasmebot.py
+++ gasmebot.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from telegram.ext import Updater, CommandHandler
 import os
 import requests
 import json
@@ -14,6 +15,7 @@ soup = BeautifulSoup(page.text, 'html.parser')
 def main(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
+    telegram_url = "https://api.telegram.org/bot{}/sendMessage?chat_id=690011658&text={}".format(token,final_text)
     return content
 
 
@@ -26,26 +28,28 @@ def priceCompare():
     text = ""
     if newPrice == "":
         text="No update yet"
-        print(text)
         return text
     elif difference > 10:
         text = "This value doesn't seem right"
-        print(text)
         return text
     elif newPrice > oldPrice:
         text= "Tomorrow's price will increase by " + str(difference) + "¢"
-        print(text)
         return text
     elif newPrice < oldPrice:
         text = "Tomorrow's price will decrease by " + str(difference) + "¢"
-        print(text)
         return text
     elif newPrice == oldPrice:
         text = "No change."
-        print("No change.")
         return text
 
 final_text = priceCompare()
-telegram_url = "https://api.telegram.org/bot{}/sendMessage?chat_id=690011658&text={}".format(token,final_text)
-#print(telegram_url)
-main(telegram_url)
+
+if __name__ == '__main__':
+    telegram_url = "https://api.telegram.org/bot{}/sendMessage?chat_id=690011658&text={}".format(token,final_text)
+    main(telegram_url)
+
+""" updater = Updater(token) #define updater
+    dp = updater.dispatcher #define dispatcher
+    dp.add_handler(CommandHandler('gasme',priceCompare))
+    updater.start_polling()
+    updater.idle()"""
