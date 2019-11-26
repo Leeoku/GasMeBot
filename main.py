6e1b2f311d626be9b1cd9324df81b0a831380fcd
Kenneth Lee <leeoku@gmail.com>
2019-11-25 21:13:23 -0500

    completed basic function of program

--- main.py
+++ main.py
@@ -8,40 +8,36 @@ soup = BeautifulSoup(page.text, 'html.parser')
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
 #oldPrice = soup.findAll("",{"id":"lPrice2"})
 #oldPrice = soup.findAll("",{"class":"gasPrice"})
 
 def main():
     updater = Updater(token,use_context=True)
-
     # register handlers
     dp = updater.dispatcher 
-    #dp.add_handler(CommandHandler("start",start))
+    # add handlers for my commands
     start_handler = CommandHandler("start",start)
     dp.add_handler(start_handler)
-    gas_handler = CommandHandler("gasme",priceCompare)
+    gas_handler = CommandHandler("gasme",gasme)
     dp.add_handler(gas_handler)
+    #set up the polling
     updater.start_polling()
     updater.idle()
     url = get_url()
     response = requests.get(url)
     content = response.content.decode("utf8")
 
-    #print(content)
-    #print(url)
-
 def start(update,context):
-    #context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-    update.message.reply_text('Hi!')
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    #update.message.reply_text('Hi!')
 
 def get_url():
     telegram_url = "https://api.telegram.org/bot{}/sendMessage?chat_id=690011658&text={}".format(token,final_text)
     return telegram_url
 
 def priceCompare():
+    #oldDate = soup.find("",{"id":"lDate2"}).text
     oldPrice = float(soup.find("",{"id":"lPrice2"}).text)
-    oldDate = soup.find("",{"id":"lDate2"}).text
     #Check to see if newPrice is updated yet
     newPrice = (soup.find("",{"id":"lPrice1"}).text)
     if newPrice == "&nbsp" or newPrice == "\xa0":
@@ -66,10 +62,13 @@ def priceCompare():
     elif newPrice < oldPrice:
         text = "Tomorrow's price will decrease by " + str(difference) + "¢"
         return text
-
+    else:
+        text = "Input error"
+        return text
 def gasme(update,context):
-    #context.bot.send _message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-    update.message.reply_text('GASSSS')
+    # This command calls the priceCompare function
+    final_text = priceCompare()
+    update.message.reply_text(final_text)
 #final_text = priceCompare()
 
 def error(update,context):
